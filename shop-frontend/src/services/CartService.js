47493fc2fc8992553f5e822d19c544f602a8834f
amitesh4u <amitesh4ever@gmail.com>
2024-06-01 11:27:44 +0530

    [REACT] Implemented Add/Remove Items from Cart

--- shop-frontend/src/services/CartService.js
+++ shop-frontend/src/services/CartService.js
@@ -4,7 +4,9 @@ const REST_API_BASE_URL = 'http://localhost:8080/shop/api/v1/carts';
 
 export const getCart = (userId) => axios.get(REST_API_BASE_URL + "/" + userId);
 
-export const addItemToCart = (userId, productId) => axios.post(REST_API_BASE_URL + "/" + userId + "/line-items?productId=" + productId);
+export const addItemToCart = (userId, productId, quantity) => axios.post(REST_API_BASE_URL + "/" + userId + "/line-items?productId=" + productId + "&&quantity=" + quantity);
+
+export const removeItemFromCart = (userId, productId, quantity) => axios.delete(REST_API_BASE_URL + "/" + userId + "/line-items?productId=" + productId + "&&quantity=" + quantity);
 
 export const deleteCart = (userId) => axios.delete(REST_API_BASE_URL + "/" + userId);
 
