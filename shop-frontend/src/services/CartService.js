f1914ba617550b1065f6849a74e7682052967a08
amitesh4u <amitesh4ever@gmail.com>
2024-06-03 20:36:01 +0530

    [REACT] Updated Product and Cart services with new end points

--- shop-frontend/src/services/CartService.js
+++ shop-frontend/src/services/CartService.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = 'http://localhost:8080/shop/api/v1/carts';
+const CART_REST_API_BASE_URL = 'http://localhost:8080/shop/api/v1/carts';
 
-export const getCart = (userId) => axios.get(REST_API_BASE_URL + "/" + userId);
+export const getCart = (userId) => axios.get(CART_REST_API_BASE_URL + "/" + userId);
 
-export const addItemToCart = (userId, productId, quantity) => axios.post(REST_API_BASE_URL + "/" + userId + "/line-items?productId=" + productId + "&&quantity=" + quantity);
+export const addItemToCart = (userId, productId, quantity) => axios.post(CART_REST_API_BASE_URL + "/" + userId + "/line-items?productId=" + productId + "&quantity=" + quantity);
 
-export const removeItemFromCart = (userId, productId, quantity) => axios.delete(REST_API_BASE_URL + "/" + userId + "/line-items?productId=" + productId + "&&quantity=" + quantity);
+export const removeItemFromCart = (userId, productId, quantity) => axios.delete(CART_REST_API_BASE_URL + "/" + userId + "/line-items?productId=" + productId + "&quantity=" + quantity);
 
-export const deleteCart = (userId) => axios.delete(REST_API_BASE_URL + "/" + userId);
+export const deleteCart = (userId) => axios.delete(CART_REST_API_BASE_URL + "/" + userId);
 
 
 
--- shop-frontend/src/services/ProductService.js
+++ shop-frontend/src/services/ProductService.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = 'http://localhost:8080/shop/api/v1/products';
+const PRODUCT_REST_API_BASE_URL = 'http://localhost:8080/shop/api/v1/products';
 
-export const listProducts = () => axios.get(REST_API_BASE_URL + "/all");
+export const listProducts = () => axios.get(PRODUCT_REST_API_BASE_URL + "/all");
 
-export const findProducts = (searchString) => axios.get(REST_API_BASE_URL + "?query=" + searchString);
+export const findProducts = (searchString) => axios.get(
+    PRODUCT_REST_API_BASE_URL + "?query=" + searchString);
+
+export const addProduct = (name, desc, qty, currency, amount) => axios.post(
+    PRODUCT_REST_API_BASE_URL + "?name=" + name + "&desc=" + desc + "&qty=" + qty
+    + "&amount=" + amount +
+    "&currency=" + currency);
+
+export const removeProduct = (productId) => axios.delete(
+    PRODUCT_REST_API_BASE_URL + "?productId=" + productId);
