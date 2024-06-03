9110e1b6d5d9628b22f215e289e60fb6f565cc50
amitesh4u <amitesh4ever@gmail.com>
2024-06-03 20:33:22 +0530

    [REACT] Added ABOUT page

---
+++ shop-frontend/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+      <div style={{minHeight:'617px'}}>
+        <h1>About</h1>
+        <p>This a Shopping application created with React 18+, JDK21, Spring Boot 3.3+ and Mysql.</p>
+      </div>
+  )
+}
+export default About
