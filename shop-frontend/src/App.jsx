2266071932b8f48e0f9accab4fd376492fe2f78e
amitesh4u <amitesh4ever@gmail.com>
2024-06-03 20:31:38 +0530

    [REACT] Implemented Router

--- shop-frontend/src/App.jsx
+++ shop-frontend/src/App.jsx
@@ -1,18 +1,31 @@
 import './App.css'
-import ListProductComponent from "./components/ListProductComponent.jsx";
-import CartComponent from "./components/CartComponent.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HeaderComponent from "./components/HeaderComponent.jsx";
 import FooterComponent from "./components/FooterComponent.jsx";
+import About from "./components/About.jsx";
+import Home from "./components/Home.jsx";
+import ListProductComponentContainer
+  from "./components/ListProductComponentContainer.jsx";
+import CartComponentContainer from "./components/CartComponentContainer.jsx";
+import AddProductComponentContainer
+  from "./components/AddProductComponentContainer.jsx";
 
 function App() {
 
   return (
-    <>
-      <HeaderComponent/>
-      <ListProductComponent/>
-      <CartComponent/>
-      <FooterComponent/>
-    </>
+      <>
+        <HeaderComponent/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/product" element={<ListProductComponentContainer/>}></Route>
+            <Route path="/cart" element={<CartComponentContainer/>}></Route>
+            <Route path="/about" element={<About/>}></Route>
+            <Route path="/addProduct" element={<AddProductComponentContainer/>}></Route>
+          </Routes>
+        </BrowserRouter>
+        <FooterComponent/>
+      </>
   )
 }
 
